a354bed0392f625775d03717593950ad1561bca0
hazhir <hazhir6690@gmail.com>
2024-07-19 12:32:33 +0330

    create a component for avatar blogs

--- src/components/pages/home/Components/Articles.tsx
+++ src/components/pages/home/Components/Articles.tsx
@@ -2,6 +2,7 @@
 import { Container, Flex } from "@/lib/styledComponents";
 import Title from "@/components/common/Title";
 import Image from "next/image";
+import AvatarCardBlogs from "@/components/common/AvatarCardBlogs";
 
 export function Articles() {
   return (
@@ -16,21 +17,16 @@ export function Articles() {
             height={198}
             className="w-full h-[198px] rounded-lg mb-6"
           />
-          <div className="flex flex-col p-1">
-            <div className="w-full  mb-10">
+          <div className="flex flex-col p-1" >
+            <div className="w-full  mb-10" >
               <span className="text-lg text-title font-semibold" >Fundamental of javascript</span >
             </div >
-            <div className="flex flex-row gap-x-4">
-              <div className="size-8 rounded-full border border-blue-400 bg-blue-500"></div >
-              <div className="flex flex-col">
-                <div className="text-start text-sm font-bold text-title">
-                  <span>hazhir</span>
-                </div >
-                <div className="text-[10px] text-paragraph">
-                  <p>Jan 10, 2022     ∙     3 min read</p>
-                </div >
-              </div >
-            </div >
+            <AvatarCardBlogs
+              author={"Hazhir"}
+              blogPost={"Jul 19, 2024"}
+              publishedAt={"3 min ago"}
+              altText={"hazhir"}
+            />
           </div >
         </Flex >
       </div >
